d6b94c7500b0908bc23c00221d7e3050d48a2333
Vishakha Dudeja <vishakha.dudeja.ext@siemens-energy.com>
2023-08-23 18:38:42 +0530

    login without header & navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
 import MySideNav from './Components/MySideNav/MySideNav';
 import DataTable from './Components/DataTable/DataTable';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <MySideNav />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/datatable' element={<DataTable />}></Route>
+    <Routes>
+        <Route element={<WithoutNav />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<WithNav />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/datatable" element={<DataTable />} />
+        </Route>
       </Routes>
-    </Router>
+      </Router>
+  );
+}
+
+function WithoutNav() {
+  return (
+    <>
+      <Outlet/>
+    </>
+  );
+}
+
+function WithNav() {
+  return (
+    <>
+      <Header />
+      <MySideNav/>
+      <Outlet />
+    </>
   );
 }
 
